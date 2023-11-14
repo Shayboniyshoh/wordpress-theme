d3556f75a68a02aa3b44a048ad6d3e1e5ece84dc
Shayboniyshoh <shayboniyshohabduvaliev@gmail.com>
2023-11-14 18:37:17 +0500

    single blog metabox

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 while (have_posts()) {
   the_post(); ?>
       <div class="post-item">
+        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
         <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
         <div class="metabox">
           <p>Posted by: <?php the_author_posts_link() ?> on <?php the_time('n-M-Y'); ?> in <?php echo get_the_category_list(', '); ?> </p>
@@ -23,5 +24,6 @@ while (have_posts()) {
         </div>
       </div>
       <?php } ?>
+      <?php echo paginate_links(); ?>
 </div>
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,13 +1,21 @@
 <?php get_header(); ?>
-<div class="page-banner">
-    <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title" style="text-align: center;"><?php echo the_title(); ?></h1>
-    </div>
-</div>
-<div class="page-banner__content container container--narrow">
+
 <?php
 while (have_posts()) {
     the_post(); ?>
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title" style="text-align: center;"><?php echo the_title(); ?></h1>
+        </div>
+    </div>
+    <div class="page-banner__content container container--narrow">
+        <div class="metabox metabox--position-up metabox--with-home-link">
+            <p><a class="metabox__blog-home-link" href="<?php echo site_url('/blogs'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blogs</a> 
+            <span class="metabox__main">Posted by: <?php the_author_posts_link() ?> on <?php the_time('n-M-Y'); ?> in <?php echo get_the_category_list(', '); ?>
+            </span>
+            </p>
+        </div>
      <p><?php the_content(); }?> </p>
 </div>
 <?php get_footer(); ?>
